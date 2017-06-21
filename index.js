1546082507424b29cd1d6f7f5a636649b2dc8d8b
Yann <yann.jego@wakanda.io>
2017-06-21 15:26:34 +0200

    Fix threadify issues

--- index.js
+++ index.js
@@ -1,117 +1,133 @@
 var DEFAULTS = {
-	NAMESPACE                : 'WAKANDA::THREADIFY',
-	QUEUE                    : true,
-	RESPONSE_TIMEOUT         : 20000,
-	CREATION_TIMEOUT         : 20000,
-	WORKER_TYPE              : 'NodeWorker',
-	MAX_WORKERS_PER_CATEGORY : 10
+	NAMESPACE: 'WAKANDA::THREADIFY',
+	RESPONSE_TIMEOUT: 20000,
+	CREATION_TIMEOUT: 20000,
+	WORKER_TYPE: 'NodeWorker',
+	MAX_WORKERS_PER_CATEGORY: 10
 };
 
 var threadify = {
-	run     : run,
-	require : _require	
+	run: run,
+	require: _require
 };
 
-function run(params){
-	var worker   = reserveWorker(params);	
+function run(params) {
+	var worker = reserveWorker(params);
+	if (worker === null) {
+		throw new Error("NO_FREE_WORKERS");
+	}
+
 	var response = runActionAndReleaseWorker(worker, params);
-	var result   = deserialize(response.type, response.data);
-	      
-	if(response.kind === 'exception'){
+	var result = deserialize(response.type, response.data);
+
+	if (response.type === 'exception') {
 		throw result;
 	}
-	
-	return result;	
+
+	return result;
 };
 
-function _require(moduleID, reserved, workerType){
-	
+function _require(moduleID, reserved, workerType) {
+
 	var modulePath = getModulePath(moduleID);
-	var _reserved  = reserved || false;
-	
+	var _reserved = reserved || false;
+
 	workerType = workerType || DEFAULTS.WORKER_TYPE;
-	
-	try{
+
+	try {
 		var _module = require(modulePath);
-	}catch(e){
+	} catch (e) {
 		throw {
-			code : 'REQUIRE_MODULE_ERROR',
-			data : {
-				moduleID   : moduleID,
-				modulePath : modulePath,
-				exception  : e
+			code: 'REQUIRE_MODULE_ERROR',
+			data: {
+				moduleID: moduleID,
+				modulePath: modulePath,
+				exception: e
 			}
 		}
 	}
-	
-	for(var propertyName in _module){
-		if(typeof _module[propertyName] === 'function'){
-			_module[propertyName] = (function(action){
-				return function(params){
+
+	for (var propertyName in _module) {
+		if (typeof _module[propertyName] === 'function') {
+			_module[propertyName] = (function (action) {
+				return function (params) {
 					return run({
-						workerType : workerType,
-						reserved   : _reserved,
-						modulePath : modulePath,
-						action	   : action,
-						data	   : params
+						workerType: workerType,
+						reserved: _reserved,
+						modulePath: modulePath,
+						action: action,
+						data: params
 					});
 				};
 			})(propertyName);
 		}
 	}
-	
+
 	return _module;
 };
 
-function runActionAndReleaseWorker(worker, params){
+function runActionAndReleaseWorker(worker, params) {
 	var port = worker.port;
 	var noResponse = true;
-	
-	port.onmessage = function(message){	
-		response	= message.data;
-		
-		var type = response.type;
+	var connected = false;
+
+	port.onmessage = function (message) {
+		response = message.data;
+
+		var kind = response.kind;
 		var data = response.data;
-		
-		if(type === '__info__' && data === 'ready'){
+
+		response.id = worker.id;
+
+		console.log(JSON.stringify(response));
+
+		if (kind === '__info__' && data === 'connected') {
 			connected = true;
-			
+
+			console.log(JSON.stringify({
+				workerID: worker.id,
+				modulePath: params.modulePath,
+				reserved: params.reserved,
+				action: params.action,
+				data: params.data
+			}));
+
 			port.postMessage({
-				modulePath : params.modulePath,
-				reserved   : params.reserved,
-				action 	   : params.action,
-				data       : params.data
+				modulePath: params.modulePath,
+				reserved: params.reserved,
+				action: params.action,
+				data: params.data
 			});
+
 		} else {
-			
-			noResponse	= false;		
-			
+			noResponse = false;
+
 			exitWait();
-		}		
+		}
 	};
-	
+
 	wait(DEFAULTS.RESPONSE_TIMEOUT);
 
 	releaseWorker(worker);
-	
-	if(noResponse){
-		console.log('TIMEOUT');
+
+	if (noResponse) {
+		console.log('TIMEOUT : ' + worker.id + ' connected : ' + connected);
 		throw {
-			code : 'TIMEOUT'
+			code: 'TIMEOUT'
 		};
 	}
-	
+
 	return response;
 }
 
-function deserialize(type, data){
+function deserialize(type, data) {
 	return data;
 }
 
-function getStorageKeyFromWorkerInfo(params){
+function getStorageKeyFromWorkerInfo(params) {
 	var key = DEFAULTS.NAMESPACE + '::' + params.workerType;
-	key+= (params.reserved) ? '::R::' + params.modulePath : '::NR';
-	
+	key += (params.reserved) ? '::R::' + params.modulePath : '::NR';
+
 	/**
 	 * Reserved     : WAKANDA::THREADIFY::NodeWorker::R::wakanda-cache-redis/redis-wakanda
 	 * Not Reserved : WAKANDA::THREADIFY::NodeWorker::NR
@@ -119,149 +135,167 @@ function getStorageKeyFromWorkerInfo(params){
 	return key;
 }
 
-function getModulePath(moduleID){
+function getModulePath(moduleID) {
 	return moduleID;
 }
 
-function reserveWorker(params){
-	
+function reserveWorker(params) {
+
 	var namespaceID = getStorageKeyFromWorkerInfo(params);
-	var worker      = null;
-	
+	var worker = null;
+
 	try {
 		storage.lock();
-			
-		var namespace 	= storage[namespaceID] || {};
-		var workers     = namespace.workers || {};
-		var freeWorkers	= workers.free || [];
+
+		var namespace = storage[namespaceID] || {};
+		var workers = namespace.workers || {};
+		var freeWorkers = workers.free || [];
 		var busyWorkers = workers.busy || [];
-		var workerInfo	= freeWorkers.pop();
-		
-		if(!workerInfo && freeWorkers.length >= DEFAULTS.MAX_WORKERS_PER_CATEGORY){
-			
+		var workerInfo = freeWorkers.pop();
+
+		if (!workerInfo && DEFAULTS.MAX_WORKERS_PER_CATEGORY && busyWorkers.length >= DEFAULTS.MAX_WORKERS_PER_CATEGORY) {
+
 			console.log('Worker Number limit reached for the namespace : ' + namespaceID);
-			
+
+			/**
+			 *  /!\ Unlock the storage before quiting the function
+			 */
+			storage.unlock();
+
 			return null;
 		} else if (!workerInfo) {
-			
+
 			console.log('Creating Worker for the namespace : ' + namespaceID);
-			
+
 			workerInfo = createWorker(params);
+		} else {
+			console.log('Found a free worker : ' + workerInfo.id);
 		}
-		
+
 		worker = getWorker(workerInfo.workerType, workerInfo.id, params.reserved, params.modulePath);
-		
-		busyWorkers.push(workerInfo);		
-		workers.free         = freeWorkers;
-		workers.busy         = busyWorkers;	
-		namespace.workers    = workers;
-		storage[namespaceID] = namespace;		
-		
-	} catch(e){
+
+		busyWorkers.push(workerInfo);
+		workers.free = freeWorkers;
+		workers.busy = busyWorkers;
+		namespace.workers = workers;
+		storage[namespaceID] = namespace;
+
+	} catch (e) {
 		console.log(e);
 	} finally {
 		storage.unlock();
 	}
-	
+
 	return worker;
 }
 
-function releaseWorker(worker){
+function releaseWorker(worker) {
 	var namespaceID = getStorageKeyFromWorkerInfo(worker);
+	console.log('Releasing worker : ' + worker.id);
+	console.log('Namespace : ' + namespaceID);
 
 	try {
 		storage.lock();
-		
-		var namespace 	= storage[namespaceID];
-		var workers     = namespace.workers;
-		var freeWorkers	= workers.free;
+
+		var namespace = storage[namespaceID];
+		var workers = namespace.workers;
+		var freeWorkers = workers.free;
 		var busyWorkers = workers.busy;
-		
+
 		var workerPosition = -1;
-		
-		busyWorkers.some(function(_worker, position){
-			if(_worker.id === worker.id){
+
+		busyWorkers.some(function (_worker, position) {
+			if (_worker.id === worker.id) {
 				workerPosition = position;
-				
+
 				return true;
 			}
 		});
-		
+
 		var workerInfo = busyWorkers.splice(workerPosition, 1)[0];
-		
+
 		freeWorkers.push(workerInfo);
-		workers.free         = freeWorkers;
-		workers.busy         = busyWorkers;	
-		namespace.workers    = workers;
+		workers.free = freeWorkers;
+		workers.busy = busyWorkers;
+		namespace.workers = workers;
 		storage[namespaceID] = namespace;
-	} catch(e){
+	} catch (e) {
 		console.log(e);
 	} finally {
 		storage.unlock();
-	}	
+	}
 }
 
-function getWorker(type, id, reserved, modulePath){
-	var workerPath	= File( File(module.filename).parent , './worker.js').path;
-	var worker		= null;
-	
-	if(type === 'NodeWorker'){
-		worker = new NodeWorker(workerPath, id);
-	} else {
+function getWorker(type, id, reserved, modulePath) {
+	var workerPath = File(File(module.filename).parent, './worker.js').path;
+	var worker = null;
+
+	if (type === 'SharedWorker') {
 		worker = new SharedWorker(workerPath, id);
+	} else {
+		worker = new NodeWorker(workerPath, id);
 	}
-	
+
+	worker.id = id;
 	worker.workerType = type;
-	worker.id         = id;
-	worker.reserved   = reserved;
+	worker.reserved = reserved;
 	worker.modulePath = modulePath;
-	
+
 	return worker;
 }
 
-function createWorker(params){
-	var id		   = generateUUID();
-	var worker	   = getWorker(params.workerType, id, params.reserved, params.modulePath);
-	var port	   = worker.port;
-	var created	   = false;
+function createWorker(params) {
+	var id = generateUUID();
+	var worker = getWorker(params.workerType, id, params.reserved, params.modulePath);
+	var port = worker.port;
+	var created = false;
+	var ready = false;
 	var workerInfo = {};
-	
-	port.onmessage = function(message){
-		var response	= message.data;
-		var type		= response.type;
-		var data		= response.data;
 
-		if(params.reserved && type === '__info__' && data === 'ready'){
-			port.postMessage({
-				action   : '__init__',
-				data     : params,
-				reserved : params.reserved
-			});
-		} else {
-			/*
-			 * Received an error or a ready
-			 * TODO : Handle errors
-			 */
-			created = true;
-			exitWait();
-		}
-		
-	};
-	
-	wait(DEFAULTS.CREATION_TIMEOUT);
-	
-	if(!created){
-		throw {
-			code    : 'TIMEOUT',
-			message : 'worker creation timeout'
-		}
-	}
-
-	workerInfo.id         = id;
+	workerInfo.id = id;
 	workerInfo.workerType = params.workerType;
 	workerInfo.modulePath = params.modulePath;
-	workerInfo.reserved   = params.reserved;
-	
+	workerInfo.reserved = params.reserved;
+
+	port.onmessage = function (message) {
+		var response = message.data;
+		var kind = response.kind;
+		var data = response.data;
+		response.workerID = id;
+		console.log(JSON.stringify(response));
+		if (kind === '__info__' && data === 'connected') {
+			created = true;
+			if (response.ready) {
+				ready = true;
+				exitWait();
+			} else {
+				console.log(JSON.stringify({
+					kind: '__info__',
+					action: 'set',
+					data: workerInfo,
+					workerID: id
+				}));
+				port.postMessage({
+					kind: '__info__',
+					action: 'set',
+					data: workerInfo
+				});
+			}
+		} else if (created && kind === 'result') {
+			ready = true;
+			exitWait();
+		}
+	};
+
+	wait(DEFAULTS.CREATION_TIMEOUT);
+
+	if (!ready) {
+		throw {
+			code: 'TIMEOUT',
+			message: 'worker creation timeout'
+		}
+	}
+	console.log("==WORKER CREATED==")
 	return workerInfo;
 }
 
--- worker.js
+++ worker.js
@@ -1,175 +1,190 @@
-var actions;
 var MAX_ACTION_WAIT = 20000;
+var info = null;
 
-onconnect = function(event){
+onconnect = function (event) {
 	var port = event.ports[0];
-	
-	port.onmessage = function(message){
-		var request 	= message.data;
-		if(!request){
+
+	port.onmessage = function (message) {
+		var request = message.data;
+		if (!request) {
 			console.log('This should not happen!');
 			sendError(port, {
-				'CODE' 		: 'INTERNAL_ERROR',
-				'message'	: 'empty request'
+				'CODE': 'INTERNAL_ERROR',
+				'message': 'empty request'
 			});
 			return;
 		}
+
+		console.log(JSON.stringify({
+			kind: request.kind,
+			action: request.action,
+			data: request.data
+		}));
+
 		handleRequest(port, request);
 	};
-	
+
 	port.postMessage({
-		'type'		: '__info__',
-		'data'		: 'ready'
+		ready: info !== null,
+		data: 'connected',
+		kind: '__info__'
 	});
 };
 
-function handleRequest(port, request){	
-	var action	 = request.action;
-	var data	 = request.data;
-	var reserved = request.reserved;
-	var _actions = actions;
-	var result;
+function handleRequest(port, request) {
+	switch (request.kind) {
+		case '__info__':
+			handleInfo(port, request);
+			break;
+		default:
+			handleAction(port, request);
+	}
+}
 
-	if(reserved && action === '__init__'){
-		try{			
-			actions = require(data.modulePath);
-			
-			if(typeof actions['__init__'] !== 'undefined'){
-				throw '"__init__" and "__info__" are reserved actions';
-			}
-			
-			if(typeof actions['init'] === 'undefined'){
-				sendResult(port, 'ready');
-				
-				return;
-			}
-			
-			if(actions.init.length >= 1){
-				actions.init(function(error){
-					if(error){
-						sendError(port, {
-							'code'	: 'RESERVED_MODULE_INIT_ERROR',
-							'data'	: error
-						});
-					}else{
-						sendResult(port, 'ready');						
-					}
-				});
-			} else {
-				actions.init();
-				sendResult(port, 'ready');
-			}
-		}catch(e){
-			console.log('An error occured during the initialization of : ' + data.module);
-			
-			sendError(port, {
-				'code'	: 'RESERVED_MODULE_INIT_ERROR',
-				'data'	: e
-			});
-		}
-		
+function handleInfo(port, request) {
+	var action = request.action;
+	var data = request.data;
+
+	switch (action) {
+		case 'set':
+			info = data;
+			sendResult(port);
+			break;
+		case 'get':
+			sendResult(port, info);
+			break;
+	}
+}
+
+function handleAction(port, request) {
+	var action = request.action;
+	var data = request.data;
+	var actions;
+
+	try {
+		actions = require(request.modulePath);
+	} catch (e) {
+		console.log('couldn\'t require the module [' + data.module + ']');
+
+		sendError(port, {
+			'code': 'MODULE_REQUIRE_ERROR',
+			'data': e
+		});
 		return;
 	}
-	
-	if(!reserved){
-		try{
-			_actions = require(request.modulePath);
-		}catch(e){
-			console.log('couldn\'t require the module [' + data.module + ']');
-			
-			sendError(port, {
-				'code'	: 'MODULE_REQUIRE_ERROR',
-				'data'	: e
-			});
-		}
-		
-		if(_actions && _actions.init && typeof(_actions.__init__ ) === "undefined"){
-			try{
-				if(_actions.init.length >= 1){
-					_actions.init(function(error){
-						if(error){
+
+	if (!actions) {
+		sendError(port, {
+			'code': 'MODULE_REQUIRE_ERROR',
+			'data': 'Empty Module'
+		});
+	}
+
+	if (actions['__inited__'] !== true) {
+		if (typeof actions['init'] === 'function') {
+			try {
+				if (actions.init.length >= 1) {
+					actions.init(function (error) {
+						if (error) {
 							sendError(port, {
-								'code'	: 'UNRESERVED_MODULE_INIT_ERROR',
-								'data'	: error
+								'code': 'MODULE_INIT_ERROR',
+								'data': error
 							});
-						} else {
-							runAction();
+
+							/**
+							 *  /!\ Quit if the initialization failed
+							 */
+							return;
 						}
 					});
 				} else {
-					_actions.init();
-					runAction();
+					actions.init();
 				}
-				
-				_actions.__init__ = true;
-			}catch(e){
+			} catch (e) {
+				console.log('An error occured during the initialization of : ' + data.module);
+
 				sendError(port, {
-					'code'	: 'UNRESERVED_MODULE_INIT_ERROR',
-					'data'	: e
+					'code': 'MODULE_INIT_ERROR',
+					'data': e
 				});
+
+				/**
+				 *  /!\ Quit if the initialization failed
+				 */
+				return;
 			}
-		} else if(_actions) {
-			runAction();
 		}
+
+		actions['__inited__'] = true;
 	}
-	
-	function runAction(){
-		if(typeof _actions[action] === 'undefined'){
+
+	runAction({ actions, port, data, action });
+}
+
+function runAction({ actions, port, data, action }) {
+	if (typeof actions[action] === 'undefined') {
+		sendError(port, {
+			'code': 'UNKNOWN_ACTION',
+			'data': {
+				'modulePath': data.modulePath,
+				'action': action
+			}
+		});
+
+		return;
+	}
+	if (actions[action].length >= 2) {
+		try {
+			actions[action](data,
+				function (error, result) {
+					if (error) {
+						sendError(port, {
+							'code': 'RUNTIME',
+							'data': error.toString()
+						});
+						return;
+					} else {
+						sendResult(port, result);
+					}
+				});
+		} catch (e) {
 			sendError(port, {
-				'code'	: 'UNKNOWN_ACTION',
-				'data'  : {
-					'modulePath' : data.modulePath,
-					'action'     : action
-				}
+				'code': 'RUNTIME',
+				'data': e.toString()
 			});
-			
 			return;
 		}
-		
-		if(_actions[action].length >= 2){
-			_actions[action](data,
-			function(error, result){
-				if(error){
-					sendError(port, {
-						'code'	: 'RUNTIME',
-						'data'	: error
-					});
-				} else {
-					sendResult(port, result);					
-				}
+	} else {
+		var result;
+		try {
+			result = actions[action](data);
+		} catch (e) {
+			sendError(port, {
+				'code': 'RUNTIME',
+				'data': e.toString()
 			});
-			
 			return;
-		} else {
-			try{
-				result = _actions[action](data);			
-			}catch(e){
-				sendError(port, {
-					'code'	: 'RUNTIME',
-					'data'	: e
-				});
-			}
-			sendResult(port, result);
 		}
+		sendResult(port, result);
 	}
 }
 
-function sendError(port, error){
-	sendMessage(port, 'exception', error);
+function sendError(port, error) {
+	sendMessage({ port, kind: 'result', type: 'exception', data: error });
 }
 
-function sendResult(port, result){
-	sendMessage(port, 'result', result);
+function sendResult(port, result) {
+	sendMessage({ port, kind: 'result', data: result });
 }
 
-function sendMessage(port, kind, data){
+function sendMessage({ port, kind, data, type }) {
 	port.postMessage({
-		'kind' : kind,
-		'type' : getTypeOfData(data),
-		'data' : data
+		'kind': kind,
+		'type': type || getTypeOfData(data),
+		'data': data
 	});
 }
 
-function getTypeOfData(data){
-	return typeof(data);
+function getTypeOfData(data) {
+	return typeof (data);
 }
